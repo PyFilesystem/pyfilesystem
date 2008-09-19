034ef7bcb036396be93ee26801b02ed9bf9b2ee1
willmcgugan <willmcgugan@67cdc799-7952-0410-af00-57a81ceafa0f>
2008-09-19 13:51:18 +0000

    Tweaking setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
+#!/user/bin/env python
+
 from distutils.core import setup
+
 from fs import __version__ as VERSION
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Python Software Foundation License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: System :: Filesystems',
+]
 
 setup(name='fs',
       version=VERSION,
-      py_modules=['fs'],
+      description="A filesytem abstraction",
+      author="Will McGugan",
+      author_email="will@willmcgugan.com",
+      url="http://code.google.com/p/pyfilesystem/",
+      packages=['fs'],
+      classifiers=classifiers,
       )
