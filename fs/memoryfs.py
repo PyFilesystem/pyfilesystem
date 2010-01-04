5391ab4bf6d838cc7b06f36bd8c53423e6cbfb63
willmcgugan <willmcgugan@67cdc799-7952-0410-af00-57a81ceafa0f>
2010-01-04 13:47:38 +0000

    Fixed dumb error

--- fs/memoryfs.py
+++ fs/memoryfs.py
@@ -180,7 +180,7 @@ class MemoryFS(FS):
         return self.dir_entry_factory(*args, **kwargs)
 
     def __init__(self, file_factory=None):
-        super(FS, self).__init__(thread_synchronize=_thread_syncronize_default)
+        super(MemoryFS, self).__init__(thread_synchronize=_thread_syncronize_default)
         self.dir_entry_factory = DirEntry
         self.file_factory = file_factory or MemoryFile
 
