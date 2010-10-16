1d9bd34e116d569017019c6f65751389d92d1fd2
rfkelly0 <rfkelly0@67cdc799-7952-0410-af00-57a81ceafa0f>
2010-10-16 08:52:59 +0000

    fix error reporting in movedir(src,dst) when src is missing

--- fs/sftpfs.py
+++ fs/sftpfs.py
@@ -301,7 +301,7 @@ class SFTPFS(FS):
             self.client.rename(nsrc,ndst)
         except IOError, e:
             if getattr(e,"errno",None) == 2:
-                raise ResourceNotFoundError(path)
+                raise ResourceNotFoundError(src)
             if self.exists(dst):
                 raise DestinationExistsError(dst)
             if not self.isdir(dirname(dst)):
