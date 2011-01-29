046942759d3e5486490048cea41ae9b23268c03e
rfkelly0 <rfkelly0@67cdc799-7952-0410-af00-57a81ceafa0f>
2011-01-29 09:49:50 +0000

    re-disable S3FS tests

--- fs/tests/test_s3fs.py
+++ fs/tests/test_s3fs.py
@@ -17,7 +17,7 @@ from fs import s3fs
 class TestS3FS(unittest.TestCase,FSTestCases,ThreadingTestCases):
 
     #  Disable the tests by default
-    #__test__ = False
+    __test__ = False
 
     bucket = "test-s3fs.rfk.id.au"
 
