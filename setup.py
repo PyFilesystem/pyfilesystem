2d20289863bff06e2e1aa7207ce7424d9508e659
willmcgugan <willmcgugan@67cdc799-7952-0410-af00-57a81ceafa0f>
2011-12-29 18:37:01 +0000

    Tweaks

--- setup.py
+++ setup.py
@@ -28,6 +28,10 @@ classifiers = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
     'Topic :: System :: Filesystems',
 ]
 
