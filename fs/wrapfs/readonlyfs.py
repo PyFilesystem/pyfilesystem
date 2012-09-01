0e3a9c8c481a5478b68428b8eeda882262294a9d
gcode@loowis.durge.org <gcode@loowis.durge.org@67cdc799-7952-0410-af00-57a81ceafa0f>
2012-09-01 11:42:13 +0000

    Prevent files being opened with '+' in the mode string. Fixes Issue 129

--- fs/wrapfs/readonlyfs.py
+++ fs/wrapfs/readonlyfs.py
@@ -40,7 +40,7 @@ class ReadOnlyFS(WrapFS):
     
     def open(self, path, mode='r', **kwargs):
         """ Only permit read access """
-        if 'w' in mode or 'a' in mode:
+        if 'w' in mode or 'a' in mode or '+' in mode:
             raise UnsupportedError('write')
         return super(ReadOnlyFS, self).open(path, mode, **kwargs)
         
